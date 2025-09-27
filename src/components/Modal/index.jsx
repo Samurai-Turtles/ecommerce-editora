805daf5e3db367321fdda41a73d165dcb850c309
Douglas Domingos <doug.domingossilva@gmail.com>
2025-09-26 21:25:38 -0300

    feat: add handleCloseModal prop to Modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,13 +1,13 @@
 import styles from "./modal.module.css";
 import { XIcon } from "@phosphor-icons/react";
 
-function Modal({ title, children }) {
+function Modal({ title, handleCloseModal, children }) {
   return (
     <div className={styles.modal}>
       <div className={styles.modalBody}>
         <span className={styles.modalTitle}>{title}</span>
         {children}
-        <button className={styles.modalCloseBtn} aria-label="Close modal">
+        <button className={styles.modalCloseBtn} aria-label="Close modal" onClick={handleCloseModal}>
           <XIcon size={24} />
         </button>
       </div>
