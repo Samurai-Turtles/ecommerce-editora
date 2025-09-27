b55ca354df1bbacfa637f6f06546660679ac8ad9
Douglas Domingos <doug.domingossilva@gmail.com>
2025-09-27 20:46:15 -0300

    feat: add base structure for cart page

---
+++ src/lib/products.js
@@ -0,0 +1,54 @@
+const productList = [
+  {
+    id: 1,
+    title: "Operational Systems: Three Easy Pieces",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, molestias!",
+    price: 9.99,
+  },
+  {
+    id: 2,
+    title: "Operational Systems: Three Easy Pieces",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, molestias!",
+    price: 9.99,
+  },
+  {
+    id: 3,
+    title: "Operational Systems: Three Easy Pieces",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, molestias!",
+    price: 9.99,
+  },
+  {
+    id: 4,
+    title: "Operational Systems: Three Easy Pieces",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, molestias!",
+    price: 9.99,
+  },
+  {
+    id: 5,
+    title: "Operational Systems: Three Easy Pieces",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, molestias!",
+    price: 9.99,
+  },
+  {
+    id: 6,
+    title: "Operational Systems: Three Easy Pieces",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, molestias!",
+    price: 9.99,
+  },
+];
+
+export function fetchProducts() {
+  return productList.map((item) => ({
+    ...item,
+    price: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(item.price),
+  }));
+}
