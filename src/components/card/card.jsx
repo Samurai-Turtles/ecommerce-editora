500e78e15139845499fb7edc39dec364fffac051
Douglas Domingos <doug.domingossilva@gmail.com>
2025-09-27 22:36:27 -0300

    feat: add horizontal style for cards

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,14 +1,28 @@
+import { clsx } from "clsx";
 import styles from "./card.module.css";
 
-function Card({ image, children, handleClick }) {
+const cardStyles = {
+  vertical: styles.verticalCard,
+  horizontal: styles.horizontalCard,
+};
+
+const cardBodyStyles = {
+  vertical: styles.verticalCardBody,
+  horizontal: styles.horizontalCardBody,
+};
+
+function Card({ style = "vertical", image, children, handleClick }) {
   return (
-    <div className={styles.cardContainer} onClick={handleClick}>
+    <div
+      className={clsx(styles.cardContainer, cardStyles[style])}
+      onClick={handleClick}
+    >
       {/* Card image */}
       <div className={styles.cardImage}>
         <img src={image} alt="Card image" />
       </div>
       {/* Card body */}
-      <div className={styles.cardBody}>{children}</div>
+      <div className={clsx(styles.cardBody, cardBodyStyles[style])}>{children}</div>
     </div>
   );
 }
